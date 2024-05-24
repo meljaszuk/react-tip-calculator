48baeea6b83e05879b054dc00fd2b28df63f4cdc
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-05-24 18:24:55 +0200

    component created

--- src/App.js
+++ src/App.js
@@ -1,61 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <form>
-      <label for="amount">
-        How much was the bill?
-      </label>
-
-      <input
-        type="text"
-        name="amount"
-        id="amunt"
-        required
-      ></input>
-
-      <br></br>
-
-      <label for="you">
-        How did you like the service?
-      </label>
-
-      <select name="you" id="you">
-        <option value="0">Week 0%</option>
-        <option value="1">Average 5%</option>
-        <option value="2">Good 10%</option>
-        <option value="3">Very Good 15%</option>
-        <option value="4">Amazing 20%</option>
-      </select>
-
-      <br></br>
-
-      <label for="friend">
-        How did your friend like the service?
-      </label>
-
-      <select name="friend" id="you">
-        <option value="0">Week 0%</option>
-        <option value="1">Average 5%</option>
-        <option value="2">Good 10%</option>
-        <option value="3">Very Good 15%</option>
-        <option value="4">Amazing 20%</option>
-      </select>
-
-      <br></br>
-
-      <div>
-        <strong>You pay X$ ($X + $Y tip)</strong>
-      </div>
-
-      <br></br>
-
-      <button>
-        Reset
-      </button>
+      <BillAmount />
+      <YourTip />
+      <FriendsTip />
+      <FinalAmount />
+      <Button />
     </form>
   );
 }
 
 export default App;
+
+function BillAmount() {
+  return (
+    <div>
+      <label htmlFor="amount">How much was the bill?</label>
+      <input type="text" name="amount" id="amount" required />
+    </div>
+  );
+}
+
+function YourTip() {
+  return (
+    <div>
+      <label htmlFor="you">How did you like the service?</label>
+      <select name="you" id="you">
+        <option value="0">Weak 0%</option>
+        <option value="1">Average 5%</option>
+        <option value="2">Good 10%</option>
+        <option value="3">Very Good 15%</option>
+        <option value="4">Amazing 20%</option>
+      </select>
+    </div>
+  );
+}
+
+function FriendsTip() {
+  return (
+    <div>
+      <label htmlFor="friend">How did your friend like the service?</label>
+      <select name="friend" id="friend">
+        <option value="0">Weak 0%</option>
+        <option value="1">Average 5%</option>
+        <option value="2">Good 10%</option>
+        <option value="3">Very Good 15%</option>
+        <option value="4">Amazing 20%</option>
+      </select>
+    </div>
+  );
+}
+
+function FinalAmount() {
+  return (
+    <div>
+      <strong>You pay X$ ($X + $Y tip)</strong>
+    </div>
+  );
+}
+
+function Button() {
+  return (
+    <button type="reset">Reset</button>
+  );
+}
